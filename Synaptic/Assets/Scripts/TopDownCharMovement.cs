68c1c467f8decb911ef900b8d3b6112c7d2e80d6
0ArcDrive0 <jgould2021@elam.co.uk>
2022-03-30 12:41:58 +0100

    Update 3.5
    4- Directional movement refined

--- Synaptic/Assets/Scripts/TopDownCharMovement.cs
+++ Synaptic/Assets/Scripts/TopDownCharMovement.cs
@@ -8,15 +8,17 @@ public class TopDownCharMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+     Animator myAnim;
+
+     float speed;
+     
+
 
     private void Awake()
     {
         _input = GetComponent < InputHandler>();
+        myAnim = GetComponent < Animator>();
     }
-
-
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +32,14 @@ public class TopDownCharMovement : MonoBehaviour
        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y); 
 
        MoveTowardTarget(targetVector);
+
+       myAnim.SetFloat("MoveX" , _input.InputVector.x);
+       myAnim.SetFloat("MoveY" , _input.InputVector.y);
     }
 
     private void MoveTowardTarget(Vector3 targetVector)
     {
-        var speed = moveSpeed * Time.deltaTime;
+        speed = moveSpeed * Time.deltaTime;
         transform.Translate(targetVector * speed);
     }
 }
