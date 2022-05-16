feb2e1704fde55a3bf5ac1d464c8b4bf47152e4b
Melissa <94051052+0ArcDrive0@users.noreply.github.com>
2022-05-16 10:33:57 +0100

    Update 35 ALT
    - Moved some things around, added more interactables and making a balcony for our game

--- Synaptic/Assets/Scripts/Dialogue/Triggers/DialogueTrigger.cs
+++ Synaptic/Assets/Scripts/Dialogue/Triggers/DialogueTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueTrigger : MonoBehaviour
 {
@@ -49,7 +50,10 @@ public class DialogueTrigger : MonoBehaviour
     {
         if (plyr.gameObject.CompareTag("Player"))
         {
-            playerInRange = false;
+         
+
+
         }
     }
+    
 }
